dc491d379ec25b0de99ac3e6e7e05663fe56e90c
Alexandra Bodak <abodak@abodak.local>
2024-07-23 14:11:25 -0400

    fix alt text

--- src/@commonsku/styles/icons/CopyIcon.tsx
+++ src/@commonsku/styles/icons/CopyIcon.tsx
@@ -39,7 +39,7 @@ type CopyIconProps = SVGIconProps;
 export default function CopyIcon({
     color=teal.main,
     size="medium",
-    altText="Meeting",
+    altText="Copy",
     ...props
 }: CopyIconProps) {
     const renderPath = "M16.5 1.5H4.5C3.4 1.5 2.5 2.4 2.5 3.5V17.5H4.5V3.5H16.5V1.5ZM19.5 5.5H8.5C7.4 5.5 6.5 6.4 6.5 7.5V21.5C6.5 22.6 7.4 23.5 8.5 23.5H19.5C20.6 23.5 21.5 22.6 21.5 21.5V7.5C21.5 6.4 20.6 5.5 19.5 5.5ZM19.5 21.5H8.5V7.5H19.5V21.5Z";
